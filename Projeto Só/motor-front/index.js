0b20b36a8074fa37436dcdfc0e839c6daf92dc70
Leticia567ds <leticiaamarallopes8@gmail.com>
2023-03-06 11:15:04 -0300

    s

--- Projeto Só/motor-front/index.js
+++ Projeto Só/motor-front/index.js
@@ -1,36 +1,53 @@
 const urListar = "http://localhost:3000/motorista";
 
-const card = document.querySelector(".card")
+// // const Button = document.querySelector(".show-content")
+var card = document.querySelector(".card");
 const corpo = document.querySelector(".box");
 var listar = [];
 
-const carregar = () => { 
-    const options = { method: "GET" }; 
-   fetch(urListar, options)
-    .then(res => res.json()) 
-   .then(res => { 
-       listar = res; 
-       lista();     
-       
-    }) 
-   .catch(err => console.error(err)); 
-   }
+function showHiddenContent(e) {
+  const input = document.querySelector("#" + e);
 
-   const lista = () => {
+  console.log("senha " + input.value);
 
-            listar.forEach(element => {
+  console.log(e);
 
-                let  lista = card.cloneNode(true)
-                lista.classList.remove("model")
+  const password = input.value;
 
-                
-                lista.querySelector("#id").innerHTML += element.id;
-                lista.querySelector("#name").innerHTML += element.nome;
-                lista.querySelector("#cnh").innerHTML +=  element.CNH;
-                lista.querySelector("#cpf").innerHTML +=  element.CPF;
-        
-     
-                corpo.appendChild(lista)
-            })
-        
+  const hiddenContent = input.parentNode.querySelector(".hidden-content");
+
+  if (password == "senha123") {
+    hiddenContent.classList.remove("model");
+  
+  } else {
+    alert("senha incorreta");
+  }
 }
+
+const carregar = () => {
+  var btn = document.querySelector("#btn");
+  const options = { method: "GET" };
+  fetch(urListar, options)
+    .then((res) => res.json())
+    .then((res) => {
+      listar = res;
+      lista();
+    })
+    .catch((err) => console.error(err));
+};
+
+const lista = () => {
+  listar.forEach((element) => {
+    let lista = card.cloneNode(true);
+    lista.classList.remove("model");
+    lista.querySelector("#id").innerHTML += element.id;
+    lista.querySelector("#name").innerHTML += element.nome;
+    lista.querySelector("#cnh").innerHTML += element.CNH;
+    lista.querySelector("#cpf").innerHTML += element.CPF;
+    lista.querySelector("#pass").setAttribute("id", "pass" + element.id);
+    lista
+      .querySelector("#btn")
+      .setAttribute("onclick", `showHiddenContent('pass${element.id}')`);
+    corpo.appendChild(lista);
+  });
+};
